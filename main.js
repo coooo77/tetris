a0baa89eeab2cf42841e9118c6f83ac511a8caf2
TL Chang <coooo77@hotmail.com>
2020-08-15 00:03:53 +0800

    create move down and freeze function

--- main.js
+++ main.js
@@ -2,28 +2,55 @@ const view = {
   creatGirds() {
     const grid = document.querySelector('.grid')
     let girds = ''
-    for (let i = 0; i < 200; i++) {
-      girds += '<div></div>'
+    for (let i = 0; i < 210; i++) {
+      if (i < 200) {
+        girds += '<div></div>'
+      } else {
+        girds += '<div class="taken"></div>'
+      }
     }
     grid.innerHTML = girds
   },
   draw() {
-    let squares = Array.from(document.querySelectorAll('.grid div'))
-    let current = model.currentTetromino()
-    current.forEach(index => squares[model.currentPosition + index].classList.add('tetromino'))
+    model.currentTetromino.forEach(index => model.squares[model.currentPosition + index].classList.add('tetromino'))
+  },
+  undraw() {
+    model.currentTetromino.forEach(index => model.squares[model.currentPosition + index].classList.remove('tetromino'))
   }
 }
 
 const controller = {
   displayGame() {
-    view.creatGirds()
+    view.creatGirds()    
+    model.currentTetromino = model.createTetromino()
+    model.squares = Array.from(document.querySelectorAll('.grid div'))
     view.draw()
+    timerId = setInterval(this.moveDown, 1000)
+  },
+  moveDown() {
+    view.undraw()
+    model.currentPosition += model.width
+    view.draw()
+    controller.freeze()
+  },
+  freeze() {
+    const currentPosition = model.currentPosition
+    const width = model.width
+    const current = model.currentTetromino
+    const squares = model.squares
+    const isCollision = current.some(index => squares[currentPosition + index + width].classList.contains('taken'))
+    if (isCollision) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      model.currentTetromino = model.createTetromino()
+      model.currentPosition = 4
+      view.draw()
+    }
   }
 }
 
 const model = {
-  tetrominoes() {
-    const width = 10
+  tetrominoes() {    
+    const width = this.width
     const tetrominoL = [
       [1, width + 1, width * 2 + 1, 2],
       [width, width + 1, width + 2, width * 2 + 2],
@@ -62,11 +89,13 @@ const model = {
       tetrominoI
     ]
   },
+  width: 10,
   currentPosition: 4,
   currentRotation: 0,
   currentTetromino() {
     return this.createTetromino()
   },
+  currentTetromino: [],
   createTetromino() {
     const index = utility.randomIndex(this.tetrominoes())
     return this.tetrominoes()[index][this.currentRotation]
